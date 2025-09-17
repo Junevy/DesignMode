17182b47bb36f2c79b86d88ab64ccd57a2b6b2c1
Junevy <junev.chung@gmail.com>
2025-09-17 21:16:16 +0800

    Update serialize attribute.

---
+++ ExtensionTechnology/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+//序列化
+SerializeViewModel svm = new() { Name = "junevy", Age = 20};
+var json = JsonSerializer.Serialize(svm);
+Console.WriteLine(json);
+
+//反序列化
+var jsonObj = JsonSerializer.Deserialize<SerializeViewModel>(json);
+Console.WriteLine(jsonObj?.Name);
+
+partial class SerializeViewModel : ObservableObject
+{
+    [ObservableProperty]
+    [property: JsonPropertyName("EN Name")] //修改序列化后键值对key的名称。
+    string name;
+
+    [ObservableProperty]
+    [property: JsonPropertyName("Expect Age")]
+    int age;
+}
